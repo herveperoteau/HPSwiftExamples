5aa02446bcc2e140554c5c5368bbb63f67ee1905
Herve Peroteau <herve.peroteau@gmail.com>
2017-03-25 18:50:32 +0100

    Setup QuizViewController

--- UITableViewRxQuiz/UITableViewRxQuiz/QuizViewController.swift
+++ UITableViewRxQuiz/UITableViewRxQuiz/QuizViewController.swift
@@ -7,13 +7,31 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class QuizViewController: UIViewController {
+class QuizViewController: UIViewController, UITableViewDelegate {
 
   @IBOutlet weak var navigationBar: UINavigationBar!
   @IBOutlet weak var nextQuestionButton: UIBarButtonItem!
   @IBOutlet weak var choiceTableView: UITableView!
   @IBOutlet weak var submitButton: UIButton!
+  
+  private let currentQuestionIndex = Variable(0)
+  private let currentQuestion: Variable<QuestionModel?> = Variable(nil)
+  private let selectedIndexPaths: Variable<[NSIndexPath]> = Variable([])
+  private let displayAnswers: Variable = Variable(false)
+  private let disposeBag = DisposeBag()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Sets self as tableview delegate
+    choiceTableView
+      .rx
+      .setDelegate(self)
+      .addDisposableTo(disposeBag)
+  }
+  
   
   
 }
