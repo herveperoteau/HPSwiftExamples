ebc75f7ea77a1ad4d1d6b0496a493b31f471c090
Herve Peroteau <herve.peroteau@gmail.com>
2017-04-19 22:00:20 +0200

    Abstract Factory Pattern

---
+++ DesignPatterns/1_CreationPatterns/6_AbstractFactoryPattern/AbstractFactory/AbstractFactory/main.swift
@@ -0,0 +1,13 @@
+var car = Car(carType: Cars.SPORTS)
+car.printDetails()
+print("-------")
+
+car = Car(carType: Cars.COMPACT)
+car.printDetails()
+print("-------")
+
+car = Car(carType: Cars.SUV)
+car.printDetails()
+print("-------")
+
+
