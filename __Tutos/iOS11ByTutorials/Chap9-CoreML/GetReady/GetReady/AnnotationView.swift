a32164a7c01748a0ee3581edb51d8106268d66d7
Hervé Péroteau <herve.peroteau@gmail.com>
2017-10-22 17:00:55 +0200

    CoreML (to determine place: forest, beach, ...) + Face Eyes & Median detection

--- __Tutos/iOS11ByTutorials/Chap9-CoreML/GetReady/GetReady/AnnotationView.swift
+++ __Tutos/iOS11ByTutorials/Chap9-CoreML/GetReady/GetReady/AnnotationView.swift
@@ -35,93 +35,166 @@ let hat = #imageLiteral(resourceName: "Park Hat")
 let glasses = #imageLiteral(resourceName: "Sunglasses")
 
 class AnnotationLayer: UIView {
-  var drawDebug = false
-
-  var faces: [FaceDimensions] = [] {
-    didSet {
-      setNeedsDisplay()
-    }
-  }
-
-  var classification: SceneType = .other {
-    didSet {
-      setNeedsDisplay()
-    }
-  }
-
-  var annotationColor: UIColor = .green
-  
-  override func draw(_ rect: CGRect) {
-    super.draw(rect)
-
-    drawFaceAnnotations()
-    drawEyeAnnotations()
-    drawMedianAnnotation()
-  }
-
-  private func drawFaceAnnotations() {
-    for face in faces.reversed() {
-      guard let faceRect = face.faceRect else { continue }
-      if drawDebug {
-        outlineFace(faceRect: faceRect)
-      }
-
-      drawHat(faceRect: faceRect)
-    }
-  }
-
-  private func drawHat(faceRect: CGRect) {
-    //Stub to be replaced
-  }
-
-  private func outlineFace(faceRect: CGRect) {
-    let path = UIBezierPath(rect: faceRect)
-    annotationColor.setStroke()
-    path.stroke()
-  }
-
-  private func drawEyeAnnotations() {
-    for face in faces {
-      if drawDebug {
-        if let left = face.leftEye {
-          outlineFeature(points: left)
+    var drawDebug = true
+    
+    var faces: [FaceDimensions] = [] {
+        didSet {
+            setNeedsDisplay()
         }
-        if let right = face.rightEye {
-          outlineFeature(points: right)
+    }
+    
+    var classification: SceneType = .other {
+        didSet {
+            setNeedsDisplay()
         }
-      }
-
-      if let left = face.leftEye, let right = face.rightEye {
-        drawGlasses(left: left, right: right)
-      }
     }
-  }
-
-  fileprivate func drawGlasses(left: [CGPoint], right: [CGPoint]) {
-    //Stub to be replaced
-  }
-
-  private func drawMedianAnnotation() {
-    for face in faces {
-      if drawDebug, let median = face.median {
-        outlineFeature(points: median)
-      }
+    
+    var annotationColor: UIColor = .green
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        drawFaceAnnotations()
+        drawEyeAnnotations()
+        drawMedianAnnotation()
     }
-  }
-
-  private func outlineFeature(points: [CGPoint], fill: Bool = false) {
-    annotationColor.setStroke()
-    annotationColor.setFill()
-    let path = UIBezierPath()
-    path.move(to: points.first!)
-    for point in points {
-      path.addLine(to: point)
+    
+    private func drawFaceAnnotations() {
+        for face in faces.reversed() {
+            guard let faceRect = face.faceRect else { continue }
+            if drawDebug {
+                outlineFace(faceRect: faceRect)
+            }
+            
+            drawHat(faceRect: faceRect, median: face.median)
+        }
     }
-    if fill {
-      path.close()
-      path.fill()
+    
+    private func drawHat(faceRect: CGRect, median: [CGPoint]?) {
+        
+        guard classification == .forest else { return }
+        
+        let hatSize = hat.size
+        let headSize = faceRect.size
+        // 1
+        let hatWidthForHead = (3.0 / 2.0) * headSize.width
+        let hatRatio = hatWidthForHead / hatSize.width
+        
+        let scaleTransform = CGAffineTransform(scaleX: hatRatio,
+                                               y: hatRatio)
+        let adjustedHatSize = hatSize.applying(scaleTransform)
+        
+        // 2
+        let hatRect = CGRect(
+            x: faceRect.midX - (adjustedHatSize.width / 2.0),
+            y: faceRect.minY - adjustedHatSize.height,
+            width: adjustedHatSize.width,
+            height: adjustedHatSize.height)
+        
+        // 3
+        //hat.draw(in: hatRect)
+        drawAngled(to: median, in: hatRect, image: hat)
     }
-    path.stroke()
-  }
-
+    
+    
+    private func outlineFace(faceRect: CGRect) {
+        let path = UIBezierPath(rect: faceRect)
+        annotationColor.setStroke()
+        path.stroke()
+    }
+    
+    private func drawEyeAnnotations() {
+        for face in faces {
+            if drawDebug {
+                if let left = face.leftEye {
+                    outlineFeature(points: left)
+                }
+                if let right = face.rightEye {
+                    outlineFeature(points: right)
+                }
+            }
+            
+            if let left = face.leftEye, let right = face.rightEye {
+                drawGlasses(left: left, right: right, median: face.median)
+            }
+        }
+    }
+    
+    private func drawGlasses(left: [CGPoint], right: [CGPoint], median: [CGPoint]?) {
+        
+        guard classification == .beach else { return }
+        
+        let total = left + right
+        let minX = total.reduce(CGFloat.infinity) { min($0, $1.x) }
+        let minY = total.reduce(CGFloat.infinity) { min($0, $1.y) }
+        let maxX = total.reduce(0) { max($0, $1.x) }
+        let maxY = total.reduce(0) { max($0, $1.y) }
+        
+        let width = max(maxX - minX, 16.0)
+        let x = (maxX - minX) / 2.0 + minX - width / 2.0
+        
+        let height = max(maxY - minY, 8.0)
+        let y = (maxY - minY) / 2.0 + minY - height / 2.0
+        
+        let eyesRect = CGRect(x: x, y: y,
+                              width: width, height: height)
+        
+        //glasses.draw(in: eyesRect)
+        drawAngled(to: median, in: eyesRect, image: glasses)
+    }
+    
+    private func drawMedianAnnotation() {
+        for face in faces {
+            if drawDebug, let median = face.median {
+                outlineFeature(points: median)
+            }
+        }
+    }
+    
+    private func outlineFeature(points: [CGPoint], fill: Bool = false) {
+        annotationColor.setStroke()
+        annotationColor.setFill()
+        let path = UIBezierPath()
+        path.move(to: points.first!)
+        for point in points {
+            path.addLine(to: point)
+        }
+        if fill {
+            path.close()
+            path.fill()
+        }
+        path.stroke()
+    }
+    
+    private func drawAngled(to median: [CGPoint]?,
+                            in accessoryRect: CGRect,
+                            image: UIImage) {
+        if let median = median, median.count >= 2 {
+            
+            let top = median.first!
+            let bottom = median.last!
+            let estimatedSlope = (top.y - bottom.y)
+                / (top.x - bottom.x )
+            let degrees = atan2(1, estimatedSlope)
+                + (estimatedSlope < 0 ? CGFloat.pi : 0)
+            
+            let context = UIGraphicsGetCurrentContext()
+            context?.saveGState()
+            
+            context?.translateBy(x: accessoryRect.midX,
+                                 y: accessoryRect.midY)
+            let angle: CGFloat = -degrees
+            context?.rotate(by: angle)
+            
+            context?.translateBy(x: -accessoryRect.width / 2 - (accessoryRect.midX - top.x),
+                                 y: -accessoryRect.height / 2)
+            let drawRect = CGRect(origin: .zero,
+                                  size: accessoryRect.size)
+            image.draw(in: drawRect)
+            
+            context?.restoreGState()
+        } else {
+            image.draw(in: accessoryRect)
+        }
+    }
 }
